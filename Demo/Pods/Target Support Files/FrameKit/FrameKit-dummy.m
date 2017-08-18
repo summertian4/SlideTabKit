c29fe814ad51f32eee92b93bb520caa09be96f09
小鱼周凌宇 <coderfish@163.com>
2017-08-18 11:07:57 +0800

    Pod 添加依赖 & pod install & 拆分 commit

---
+++ Demo/Pods/Target Support Files/FrameKit/FrameKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FrameKit : NSObject
+@end
+@implementation PodsDummy_FrameKit
+@end
---
+++ Demo/Pods/Target Support Files/Pods-SlideTabKitDemoTests/Pods-SlideTabKitDemoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SlideTabKitDemoTests : NSObject
+@end
+@implementation PodsDummy_Pods_SlideTabKitDemoTests
+@end
